a65099ee0778590ac8f182388677aded895190f2
z3lovebaby <phu24012002@gmail.com>
2023-05-02 15:37:39 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const express = require('express')
+const { engine } = require ('express-handlebars');
+const app = express()
+const port = 3000
+app.use(express.static(path.join(__dirname,'public')))
+app.engine('hbs', engine({
+  extname:'.hbs'
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', 'src\\resource\\views');
+app.get('/', (req, res) => {
+  res.render('home');
+})
+app.get('/t', (req, res) => {
+  res.render('news');
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
